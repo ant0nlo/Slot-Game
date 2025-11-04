e953e1a541b8771c47231aefab2d8c82f1f5e237
antonio <antoniopenev23@gmail.com>
2025-11-04 12:56:10 +0200

    Update README and documentation

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,16 @@ try {
   const reelGen = new ReelGenerator(config.rowsCount);
   const payoutEval = new PayoutEvaluator(config.symbols);
   const slot = new SlotMachine(config.reels, config.lines, reelGen, payoutEval);
-
+  
   const result = slot.spin();
 
+  function transpose<T>(matrix: T[][]): T[][] {
+  return matrix[0].map((_, i) => matrix.map(row => row[i]));
+  }
+
   console.log("=== SLOT RESULT ===");
-  console.table(result.screen);
+  //console.table(result.screen);
+  console.table(transpose(result.screen));
   console.log("\n=== LINE RESULTS ===");
   console.table(result.linesResult);
   console.log(`\nTOTAL WIN: ${result.totalWin}`);
